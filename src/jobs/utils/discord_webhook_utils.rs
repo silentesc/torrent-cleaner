0fb4648b12e16fb1437a8ba97a0c336ccd173651
silentesc <scope-frisk-copy@duck.com>
2025-12-07 00:13:03 +0100

    further improve cooldown system for discord messages

--- src/jobs/utils/discord_webhook_utils.rs
+++ src/jobs/utils/discord_webhook_utils.rs
@@ -37,13 +37,15 @@ impl DiscordWebhookUtils {
         return self.discord_webhook_url.is_some();
     }
 
-    async fn get_retry_after_millis(&self, headers: &HeaderMap) -> u64 {
+    async fn get_retry_after_millis(&mut self, headers: &HeaderMap) -> u64 {
         let timestamp_secs_now = Local::now().timestamp();
         let fail_count_in_last_10_secs = self.request_history.iter().filter(|(timestamp_secs, is_success)| (timestamp_secs_now - 10 <= *timestamp_secs) && !*is_success).count();
-        if fail_count_in_last_10_secs >= 5 {
-            Logger::warn("5+ requests to discord failed in the last 10 seconds, waiting 60 seconds to cooldown...");
+        if fail_count_in_last_10_secs >= 4 {
+            Logger::warn("4 or more requests to discord failed in the last 10 seconds, waiting 60 seconds to cooldown...");
+            self.request_history.clear();
             sleep(Duration::from_secs(60)).await;
             Logger::info("Cooled down, continuing...");
+            return 0;
         }
 
         // Try get header_value
@@ -77,40 +79,42 @@ impl DiscordWebhookUtils {
     }
 
     async fn make_request(&mut self, payload: &Value) -> Result<(), anyhow::Error> {
-        if let Some(discord_webhook_url) = &self.discord_webhook_url {
-            loop {
-                match self.client.post(discord_webhook_url.clone()).json(payload).send().await {
-                    // Got a response
-                    Ok(response) => {
-                        // Success
-                        if response.status().is_success() {
-                            self.request_history.push((Local::now().timestamp(), true));
-                            break;
-                        }
-                        // Error: 429
-                        else if response.status() == StatusCode::TOO_MANY_REQUESTS {
-                            self.request_history.push((Local::now().timestamp(), false));
-                            let retry_after_millis = self.get_retry_after_millis(&response.headers()).await;
-                            if retry_after_millis > 0 {
-                                Logger::warn(format!("Received status code 429 (too many requests) from discord, waiting {:.2} seconds", (retry_after_millis as f64 / 1000.0)).as_str());
-                                sleep(Duration::from_millis(retry_after_millis)).await;
-                            }
-                        }
-                        // Error: Unexpected
-                        else {
-                            self.request_history.push((Local::now().timestamp(), false));
-                            return Err(anyhow::anyhow!(
-                                "Sending discord notification failed with status code {}: {}",
-                                response.status(),
-                                response.text().await.unwrap_or_default(),
-                            ));
-                        }
+        let discord_webhook_url = match self.discord_webhook_url.clone() {
+            Some(discord_webhook_url) => discord_webhook_url,
+            None => return Ok(()),
+        };
+        loop {
+            match self.client.post(discord_webhook_url.clone()).json(payload).send().await {
+                // Got a response
+                Ok(response) => {
+                    // Success
+                    if response.status().is_success() {
+                        self.request_history.push((Local::now().timestamp(), true));
+                        break;
                     }
-                    // Request itself failed
-                    Err(e) => {
+                    // Error: 429
+                    else if response.status() == StatusCode::TOO_MANY_REQUESTS {
                         self.request_history.push((Local::now().timestamp(), false));
-                        return Err(anyhow::anyhow!("Sending discord notification failed: {:#}", e));
+                        let retry_after_millis = self.get_retry_after_millis(&response.headers()).await;
+                        if retry_after_millis > 0 {
+                            Logger::warn(format!("Received status code 429 (too many requests) from discord, waiting {:.2} seconds", (retry_after_millis as f64 / 1000.0)).as_str());
+                            sleep(Duration::from_millis(retry_after_millis)).await;
+                        }
                     }
+                    // Error: Unexpected
+                    else {
+                        self.request_history.push((Local::now().timestamp(), false));
+                        return Err(anyhow::anyhow!(
+                            "Sending discord notification failed with status code {}: {}",
+                            response.status(),
+                            response.text().await.unwrap_or_default(),
+                        ));
+                    }
+                }
+                // Request itself failed
+                Err(e) => {
+                    self.request_history.push((Local::now().timestamp(), false));
+                    return Err(anyhow::anyhow!("Sending discord notification failed: {:#}", e));
                 }
             }
         }
