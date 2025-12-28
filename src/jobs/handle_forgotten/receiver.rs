42bbc092b61bcd73a7878b398391d041252ba649
silentesc <scope-frisk-copy@duck.com>
2025-12-28 21:07:47 +0100

    Throw error if checking for external hardlinks fails

--- src/jobs/handle_forgotten/receiver.rs
+++ src/jobs/handle_forgotten/receiver.rs
@@ -30,7 +30,8 @@ impl Receiver {
         Logger::debug(Category::HandleForgotten, "Checking torrents for criteria...");
         let mut torrents_criteria: HashMap<String, (Torrent, bool)> = HashMap::new();
         for torrent in &torrents {
-            torrents_criteria.insert(torrent.hash().to_string(), (torrent.clone(), Receiver::is_criteria_met(&torrent, &known_hardlinks, config)));
+            let is_criteria_met = Receiver::is_criteria_met(&torrent, &known_hardlinks, config)?;
+            torrents_criteria.insert(torrent.hash().to_string(), (torrent.clone(), is_criteria_met));
         }
         Logger::debug(Category::HandleForgotten, "Done checking torrents for criteria");
 
@@ -40,54 +41,39 @@ impl Receiver {
     /**
      * Is criteria met
      */
-    fn is_criteria_met(torrent: &Torrent, known_hardlinks: &HashMap<u64, u64>, config: &Config) -> bool {
+    fn is_criteria_met(torrent: &Torrent, known_hardlinks: &HashMap<u64, u64>, config: &Config) -> Result<bool, anyhow::Error> {
         // Uncompleted
         if *torrent.completion_on() == -1 {
-            Logger::trace(Category::HandleForgotten, format!("Torrent doesn't meet criteria (uncompleted): ({}) {}", torrent.hash(), torrent.name(),).as_str());
-            return false;
+            Logger::trace(Category::HandleForgotten, format!("Torrent doesn't meet criteria (uncompleted): ({}) {}", torrent.hash(), torrent.name()).as_str());
+            return Ok(false);
         }
         // Protection tag
         if torrent.tags().contains(config.jobs().handle_forgotten().protection_tag()) {
-            Logger::trace(Category::HandleForgotten, format!("Torrent doesn't meet criteria (protection tag): ({}) {}", torrent.hash(), torrent.name(),).as_str());
-            return false;
+            Logger::trace(Category::HandleForgotten, format!("Torrent doesn't meet criteria (protection tag): ({}) {}", torrent.hash(), torrent.name()).as_str());
+            return Ok(false);
         }
         // Seed time
         let seeding_days = torrent.seeding_time() / 60 / 60 / 24;
-        if seeding_days < config.jobs().handle_forgotten().min_seeding_days() as i64 {
+        let min_seeding_days = config.jobs().handle_forgotten().min_seeding_days() as i64;
+        if seeding_days < min_seeding_days {
             Logger::trace(
                 Category::HandleForgotten,
-                format!(
-                    "Torrent doesn't meet criteria (minimum seed day limit {}/{}): ({}) {}",
-                    seeding_days,
-                    config.jobs().handle_forgotten().min_seeding_days(),
-                    torrent.hash(),
-                    torrent.name(),
-                )
-                .as_str(),
+                format!("Torrent doesn't meet criteria (minimum seed day limit {}/{}): ({}) {}", seeding_days, min_seeding_days, torrent.hash(), torrent.name()).as_str(),
             );
-            return false;
+            return Ok(false);
         }
         // Media library
-        match FileUtils::has_external_hardlinks(known_hardlinks, torrent.content_path()) {
-            Ok(has_torrent_external_hardlinks) => {
-                if has_torrent_external_hardlinks {
-                    Logger::trace(
-                        Category::HandleForgotten,
-                        format!("Torrent doesn't meet criteria (has external hardlink): ({}) {}", torrent.hash(), torrent.name(),).as_str(),
-                    );
-                    return false;
-                }
-            }
-            Err(e) => {
-                Logger::error(
-                    Category::HandleForgotten,
-                    format!("Torrent doesn't meet criteria (error while checking if has external hardlink): ({}) {}: {:#}", torrent.hash(), torrent.name(), e,).as_str(),
-                );
-                return false;
-            }
+        let has_external_hardlinks = FileUtils::has_external_hardlinks(known_hardlinks, torrent.content_path())?;
+        if has_external_hardlinks {
+            Logger::trace(
+                Category::HandleForgotten,
+                format!("Torrent doesn't meet criteria (has external hardlink): ({}) {}", torrent.hash(), torrent.name()).as_str(),
+            );
+            return Ok(false);
         }
 
-        Logger::trace(Category::HandleForgotten, format!("Torrent meets criteria: ({}) {}", torrent.hash(), torrent.name(),).as_str());
-        true
+        Logger::trace(Category::HandleForgotten, format!("Torrent meets criteria: ({}) {}", torrent.hash(), torrent.name()).as_str());
+
+        Ok(true)
     }
 }
