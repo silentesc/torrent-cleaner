c3174519f23a7368fbc0f23b93c32ca679a6039c
silentesc <scope-frisk-copy@duck.com>
2026-01-02 03:47:08 +0100

    Job Scheduler now saves last run datetime in db so startup sleep time is calculated accordingly

--- src/utils/date_utils.rs
+++ src/utils/date_utils.rs
@@ -1,6 +1,7 @@
-use chrono::{Local, NaiveDate, ParseError};
+use chrono::{Local, NaiveDate, NaiveDateTime, ParseError};
 
 static DATE_FORMAT: &str = "%Y-%m-%d";
+static DATETIME_FORMAT: &str = "%Y-%m-%d %H:%M:%S";
 
 pub struct DateUtils;
 
@@ -9,11 +10,23 @@ impl DateUtils {
         Local::now().date_naive()
     }
 
+    pub fn get_current_local_naive_datetime() -> NaiveDateTime {
+        Local::now().naive_local()
+    }
+
     pub fn convert_naive_date_to_string(naive_date: NaiveDate) -> String {
         naive_date.format(DATE_FORMAT).to_string()
     }
 
+    pub fn convert_naive_datetime_to_string(naive_datetime: NaiveDateTime) -> String {
+        naive_datetime.format(DATETIME_FORMAT).to_string()
+    }
+
     pub fn parse_naive_date_from_str(str: &str) -> Result<NaiveDate, ParseError> {
         NaiveDate::parse_from_str(str, DATE_FORMAT)
+    }
+
+    pub fn parse_naive_datetime_from_str(str: &str) -> Result<NaiveDateTime, ParseError> {
+        NaiveDateTime::parse_from_str(str, DATETIME_FORMAT)
     }
 }
